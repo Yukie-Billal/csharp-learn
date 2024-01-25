39e9cc247d7f4116f63c82298ef4b4a18dc37eb2
Yukie M Billal <yukiembillal01@gmail.com>
2024-01-25 23:57:24 +0700

    initial commit

---
+++ journey/Program.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Never make a new line. ")
+        Console.WriteLine("Second text: This not in new line, but output after this will display in new line")
+        Console.WriteLine("Last line in new line")
+
+	string name = "Yukie muhammad billal"
+	string address = "indonesia" , print_var = "You event can print a variabel"
+	int age = 19
+	double height = 155.5
+	float f_height = 155.5f // user less memory, postfix f
+	decimal d_height = 155.5m // user high memory, postfix m
+	char first_letter = 'a', last_letter = '1'
+	bool isLife = true, kekkon = false, kanojo = false
+
+	Console.WriteLine(print_var)
+    }
+}
